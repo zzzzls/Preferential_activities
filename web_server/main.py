abf1ef6684a29ece34cb3dab5917509febf28e7f
zzzzls <245129129@qq.com>
2021-03-09 21:02:28 +0800

    NEW Feature: 搜索功能

--- web_server/main.py
+++ web_server/main.py
@@ -1,5 +1,5 @@
 import pymysql
-from flask import Flask, g, make_response, jsonify
+from flask import Flask, g, make_response, jsonify, request, redirect
 
 app = Flask(__name__)
 
@@ -37,15 +37,9 @@ def after_request(environ):
     return environ
 
 
-@app.route('/query_new_data')
-def get_news():
-    data = query_data_from_mysql(g.cur)
-    if data and len(data) > 0:
-        result = {'codes': 200, "data": data}
-    else:
-        result = {'codes': 500, "data": []}
-
-    response = make_response(jsonify(result))
+def customize_response_cros(data):
+    """定制跨域响应体"""
+    response = make_response(jsonify(data))
     # 设置响应请求头
     response.headers["Access-Control-Allow-Origin"] = '*'  # 允许使用响应数据的域。也可以利用请求header中的host字段做一个过滤器。
     response.headers["Access-Control-Allow-Methods"] = 'POST'  # 允许的请求方法
@@ -54,17 +48,37 @@ def get_news():
     return response
 
 
-def query_data_from_mysql(cur):
-    sql = 'SELECT title,url,releaseTime FROM news ORDER BY releaseTime DESC limit 40'
+@app.route('/query_data')
+def get_news():
+    args = request.args
+    kw = args.get('kw', '').strip()
+    # 检测用户是否有输入内容
+    if kw == '':
+        data = query_data_from_mysql(g.cur)
+    else:
+        data = query_data_from_mysql(g.cur, search_key=kw, time_fmt='%m-%d %H:%M')
+    if data and len(data) > 0:
+        result = {'codes': 200, "data": data}
+    else:
+        result = {'codes': 500, "data": []}
+
+    return customize_response_cros(result)
+
+
+def query_data_from_mysql(cur, search_key=None, limit=30, time_fmt='%H:%M'):
+    if not search_key:
+        sql = f'SELECT title,url,releaseTime FROM news ORDER BY releaseTime DESC limit {limit}'
+    else:
+        sql = f'SELECT title,url,releaseTime FROM news where LOCATE("{search_key}", title)>0 ORDER BY releaseTime DESC limit {limit}'
     cur.execute(sql)
     result = cur.fetchall()
     new_result = []
     for item in list(result):
         title = item[0][:37] + '...' if len(item[0]) > 39 else item[0]
-        releaseTime = item[2].strftime('%H:%M')
+        releaseTime = item[2].strftime(time_fmt)
         new_result.append([title, item[1], releaseTime])
     return new_result
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=8090)
